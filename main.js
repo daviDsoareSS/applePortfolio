b5233def1a7b38d26b154941d7edefb9a07ce082
daviDsoareSS <david.silva425@etec.sp.gov.br>
2024-01-02 23:05:02 -0300

    itens atualizados

--- main.js
+++ main.js
@@ -17,3 +17,36 @@ buttonChangeColorBody.addEventListener('click', function(){
 
   }
 })
+
+const token = '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'; // Substitua com seu token pessoal
+const gridCardsRepos = document.querySelector('.grid-projects');
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    // Aqui você pode trabalhar com os dados dos repositórios
+    console.log(data);
+
+    data.forEach(element => {
+      let card = document.createElement('div');
+      card.className = 'card-project';
+      
+      if(element.language == 'JavaScript'){
+        let cardHTML = 
+        `<p>${element.id}</p>` +
+        `<small>${element.name}</small>` +
+        `<p>${element.language}</p>`;
+
+        card.innerHTML = cardHTML;
+        
+        gridCardsRepos.appendChild(card);
+                       
+      }
+      
+    });
+  })
+  .catch((error) => console.error('Erro:', error));
